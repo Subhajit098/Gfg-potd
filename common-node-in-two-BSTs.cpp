5ff080c69c220128244a1714bf1525b526dae464
androidjit07 <sd.20u10020@btech.nitdgp.ac.in>
2023-10-14 22:22:44 +0530

    common-node-in-two-BSTs -> 14/10/23

---
+++ common-node-in-two-BSTs.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Time Complexity : O(h1) + O(h2) +  O(max(h1,h2))
+// Space Complexity : O(max(h1,h2))
+
+
+void traversal(Node* root,map<int,int>&mp){
+        if(!root) return;
+        
+        mp[root->data]++;
+        traversal(root->left,mp);
+        traversal(root->right,mp);
+    }
+    
+    vector <int> findCommon(Node *root1, Node *root2)
+    {
+     //Your code here
+     map<int,int>mp;
+     traversal(root1,mp);
+     traversal(root2,mp);
+     vector<int>ans;
+     for(auto it:mp){
+         if(it.second>1) ans.push_back(it.first);
+     }
+     return ans;
+    }
