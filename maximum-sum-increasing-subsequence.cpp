c98caaadbe3efac39ff53950885d89d583e4ad8b
androidjit07 <sd.20u10020@btech.nitdgp.ac.in>
2023-10-24 20:07:22 +0530

    max-sum-inc-subseq , dp , recursion -> 24/10/23

---
+++ maximum-sum-increasing-subsequence.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxSumIS(int arr[],int n){
+    int dp[n];
+    for(int i=0;i<n;i++) dp[i]=arr[i];
+
+    for(int i=1;i<n;i++){
+        for(int j=0;j<i;j++){
+            if(arr[i]>arr[j] && arr[i]+dp[j]>dp[i]) dp[i]=arr[i]+dp[j];
+        }
+    }
+
+    return *max_element(dp,dp+n);
+}
