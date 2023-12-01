13126d31ab8d1e42e96328346683788c4c3c96a8
androidjit07 <sd.20u10020@btech.nitdgp.ac.in>
2023-12-01 16:27:47 +0530

    shortest path from 1 to n -> 1/12/23

---
+++ shortest-path-from-1-to-n.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int minimumStep(int n){
+        //complete the function here
+        int ans=0;
+        while(n!=1){
+            if(n%3==0) n=n/3;
+            else n--;
+            ans++;
+        }
+        return ans;
+    }
